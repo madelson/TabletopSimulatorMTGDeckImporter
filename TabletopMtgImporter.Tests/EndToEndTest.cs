41e53c47f85d8514af13b6d549a279c77291766c
Michael Adelson <mike.adelson314@gmail.com>
2020-05-13 06:50:23 -0400

    Add hashing regression test

--- TabletopMtgImporter.Tests/EndToEndTest.cs
+++ TabletopMtgImporter.Tests/EndToEndTest.cs
@@ -1,7 +1,9 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +11,11 @@ namespace TabletopMtgImporter.Tests
 {
     public class EndToEndTest
     {
-        [TestCase("Archidekt1CardNameFormat.txt")]
-        [TestCase("Archidekt1xCardNameCodeCategoryLabel.txt")]
-        [TestCase("MaybeboardAndAlternateArtCollectorNumber.txt")]
-        [TestCase("ComboPieceRelatedCards.txt")]
-        public async Task TestRunsEndToEndWithoutErrors(string sampleName)
+        [TestCase("Archidekt1CardNameFormat.txt", ExpectedResult = "qVp87K8AISOkZVF1RM5Yiw==")]
+        [TestCase("Archidekt1xCardNameCodeCategoryLabel.txt", ExpectedResult = "7gVfD5APapb1UJW44Vu9gQ==")]
+        [TestCase("MaybeboardAndAlternateArtCollectorNumber.txt", ExpectedResult = "ldTApaFLOzy1MT1Yu1zj4Q==")]
+        [TestCase("ComboPieceRelatedCards.txt", ExpectedResult = "lvnt9ZY3tnYbuhrlf3v0RA==")]
+        public async Task<string> TestRunsEndToEndWithoutErrors(string sampleName)
         {
             using var sample = SamplesHelper.GetSample(sampleName);
 
@@ -22,6 +24,13 @@ namespace TabletopMtgImporter.Tests
             Assert.IsTrue(await importer.TryImportAsync(new DeckFileInput(sample.Path)));
             Assert.IsEmpty(testLogger.ErrorLines);
             Assert.IsEmpty(testLogger.WarningLines);
+
+            var outputFile = Path.Combine(TestConfiguration.Configuration.OutputDirectory, Path.ChangeExtension(Path.GetFileName(sample.Path), ".json"));
+            Assert.IsTrue(File.Exists(outputFile));
+
+            using var md5 = MD5.Create();
+            var hash = Convert.ToBase64String(md5.ComputeHash(File.ReadAllBytes(outputFile)));
+            return hash;
         }
     }
 }
