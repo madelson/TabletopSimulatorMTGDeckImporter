681e7e55871e2eb5355624a3ba3129fdef166a7c
madelson <1269046+madelson@users.noreply.github.com>
2026-04-29 19:20:23 -0400

    Bump timeout again

--- TabletopMtgImporter.Core/ScryfallClient.cs
+++ TabletopMtgImporter.Core/ScryfallClient.cs
@@ -45,7 +45,7 @@ namespace TabletopMtgImporter
             this._logger.Info($"Downloading {url}");
 
             // rate-limiting requested by scryfall (they want < 10/sec)
-            await Task.Delay(TimeSpan.FromMilliseconds(150)).ConfigureAwait(false);
+            await Task.Delay(TimeSpan.FromMilliseconds(500)).ConfigureAwait(false);
 
             using var response = await HttpClient.GetAsync(url).ConfigureAwait(false);
             if (!response.IsSuccessStatusCode)
